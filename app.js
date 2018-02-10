43b2eb4097cadd81ca2a7c56b6e1951abce8a87b
Yan MATAGNE <ymatagne@norsys.fr>
2018-02-10 11:28:21 -0500

    chore(front) : init project

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+<<<<<<< HEAD
 app.use('/api/v1/users', users);
 
+=======
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/api/v1/users', users);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+>>>>>>> chore(front) : init project
 module.exports = app;
