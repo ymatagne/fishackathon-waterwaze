404b4e863a6292c62ddb2b3901a2e9cbc7820eba
John Goff <john.g@hiredhippo.ai>
2018-02-10 13:03:09 -0500

    import needed mongoose model and passport in routes/users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const passport = require('passport');
+const User = require('../models/user');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
